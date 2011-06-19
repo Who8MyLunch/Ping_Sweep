a27b94559ef242e7b481fe46d4406343888d4890
Pierre Villeneuve <pierre.villeneuve@gmail.com>
2011-06-18 21:27:16 -0700

    Refactored text display functions.  Now print output line as results are available.  Don't wait until the very end.

--- ping_sweep.py
+++ ping_sweep.py
@@ -19,6 +19,7 @@
 
 from __future__ import division, print_function #, unicode_literals
 
+import os
 import sys
 import time
 import socket
@@ -31,8 +32,16 @@ import dpkt
 def percentile(data, P):
     data.sort()
     
-    ix = P*len(data)
-    val = data[ix]
+    if not hasattr(P, '__iter__'):
+        P = [P]
+        
+    val = []
+    for perc in P:
+        ix = int(perc*len(data))
+        if ix == len(data):
+            ix -= 1
+                    
+        val.append(data[ix])
         
     return val
 
@@ -103,11 +112,17 @@ def create_socket(host_name, timeout=None):
 
 
 
-def ping_once(sock, data_size=32, id=1):
+def ping_once(sock, data_size=None, id=None):
     """
     One ping, just one ping.
     """
 
+    if data_size is None:
+        data_size = 64
+        
+    if id is None:
+        id = 1
+        
     seq = 1999 # not really used here, but the TV show Space 1999! was pretty awesome when I was a kid.
 
     payload, packet = create_packet(id, seq, data_size)
@@ -137,6 +152,7 @@ def ping_once(sock, data_size=32, id=1):
     # Done.
     result = {'time_ping':time_ping,
               'data_size':data_size,
+              'timeout':sock.gettimeout()*1000.,  # convert from seconds to milliseconds
               'is_same_data':is_same_data,
               'id':id,
               'echo_id':echo_id}
@@ -145,7 +161,7 @@ def ping_once(sock, data_size=32, id=1):
 
 
 
-def ping_repeat(host_name, data_size=64, time_pause=25., count_send=10, timeout=None):
+def ping_repeat(host_name, data_size=None, time_pause=None, count_send=None, timeout=None):
     """
     Ping remote host.  Repeat for better statistics.
 
@@ -155,22 +171,27 @@ def ping_repeat(host_name, data_size=64, time_pause=25., count_send=10, timeout=
     timeout: socket timeout period, seconds.
     """
 
+    if time_pause is None:
+        time_pause = 5.  # milliseconds
+        
+    if count_send is None:
+        count_send = 25
+        
+    if timeout is None:
+        timeout = 5000.  # ms
+        
     # Make a socket, send a sequence of pings.
-    sock = create_socket(host_name, timeout=timeout)
+    sock = create_socket(host_name, timeout=timeout/1000.)   # note: timeout in seconds, not milliseconds.
     time_sweep_start = now()
     time_sleeping = 0.
     results = []
     for k in range(count_send):
         if k > 0:
             # Little pause between sending packets.  Try to be a little nice.
-            time_mark = now()
             time.sleep(time_pause / 1000.)  # sleep in seconds, not milliseconds.
-            time_sleeping += (now() - time_mark)
 
         res = ping_once(sock, data_size=data_size)
         results.append(res)
-
-    time_sweep_end = now()
 
     # Process results.
     count_timeout = 0
@@ -196,18 +217,14 @@ def ping_repeat(host_name, data_size=64, time_pause=25., count_send=10, timeout=
     # num_packets = len(times)
     data_size = results[0]['data_size']
 
-    # debugging.
-    print(data_size)
-
-    rate = count_recv * data_size / (time_sweep_end - time_sweep_start - time_sleeping) / 1024.
-
     P = [0.00, 0.25, 0.50, 0.75, 1.00]
     P_times = percentile(times, P)
 
     stats = {'host_name':host_name,
              'data_size':data_size,
-             'rate':rate,
              'times':times,
+             'timeout':res['timeout'],
+             'time_pause':time_pause,
              'P':P,
              'P_times':P_times,
              'count_send':count_send,
@@ -220,73 +237,90 @@ def ping_repeat(host_name, data_size=64, time_pause=25., count_send=10, timeout=
 
 
 
-def ping_sweep(host_name, size_sweep=None, time_pause=25, count_send=10):
+def ping_sweep(host_name, timeout=None, size_sweep=None, time_pause=None, count_send=None, verbosity=False):
     """
     Perform a sequence of pings over a range of payload sizes.
     """
     if size_sweep is None:
-        # size_sweep = [32, 64, 128] # basic
-        size_sweep = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768]
-        # size_sweep = [2048, 4090, 4093, 4096, 4099, 4102]
-        # size_sweep = [16, 32, 64, 128, 256, 512, 1024, 2048, 8192, 16384, 32768]
+        size_sweep = [32, 128, 512, 2048]
 
     stats_sweep = []
     for s in size_sweep:
-        stats = ping_repeat(host_name, data_size=s, time_pause=time_pause, count_send=count_send)
+        stats = ping_repeat(host_name, data_size=s,
+                            timeout=timeout, 
+                            time_pause=time_pause,
+                            count_send=count_send)
         stats_sweep.append(stats)
 
+        if verbosity:
+            if len(stats_sweep) == 1:
+                display_results_header(stats)
+            display_results_line(stats)
+            
     # Done.
     return stats_sweep
 
 
 
-def pretty_results(stats_sweep):
+def display_results_header(stats):
     """
     Nice results display.
     """
 
     # Print.
-    print('\n\n Ping sweep target: %s' % stats_sweep[0]['host_name'])
-    print(' count_send=%d\n' % stats_sweep[0]['count_send'])
-
+    print()
+    print(' Ping Sweep')
+    print(' ==========')
+    print(' target name:  %s' % stats['host_name'])
+    print(' ping count:   %d' % stats['count_send'])
+    print(' sock timeout: %d ms' % stats['timeout'])
+    print(' pause time:   %d ms' % stats['time_pause'])
+    print()
+    
     # Percentiles.
-    P = stats_sweep[0]['P']
+    P = stats['P']
 
-    # Header.
-    head = '  size '
+    # Header string.
+    head_top = '  Packet   Percentile Times (ms)              Lost Packets'
+    head_bot = '   Size '
     for p in P:
-        head += '   %4.2f' % p
-    head += '   Lost TO  Crpt  Rate'
+        head_bot += ' %6.2f' % p
+    head_bot += '    All [ T / C ]'
 
-    print(head)
-    div = ' ' + '-' * (len(head)-1)
+    print(head_top)
+    print(head_bot)
+
+    div = ' ' + '-' * (len(head_bot)-1)
     print(div)
 
+
+
+def display_results_line(stats):
+    """
+    Generate line of text for current set of results.
+    """
     # Line output.
-    template = ' %5d '
-    for p in P:
+    template = ' %5d  '
+    for p in stats['P']:
         template += ' %6.2f'
 
-    template += '   %3d %3d %3d   %5.1f'
+    template += '   %3d  %3d %3d'
 
-    for stats in stats_sweep:
-        num_bytes = stats['data_size']
+    num_bytes = stats['data_size']
 
-        P_times = stats['P_times']
-        val = [num_bytes]
-        for p in P_times:
-            val.append(p*1000.)
+    P_times = stats['P_times']
+    val = [num_bytes]
+    for p in P_times:
+        val.append(p*1000.)
 
-        val.append(stats['count_lost'])
-        val.append(stats['count_timeout'])
-        val.append(stats['count_corrupt'])
-        val.append(stats['rate'])
-        val = tuple(val)
+    val.append(stats['count_lost'])
+    val.append(stats['count_timeout'])
+    val.append(stats['count_corrupt'])
+    val = tuple(val)
 
-        print(template % val)
-
-
-
+    print(template % val)
+    
+    
 
 if __name__ == '__main__':
     # Example useage.
@@ -300,10 +334,11 @@ if __name__ == '__main__':
         # host_name = 'vulture'
         # host_name = 'purple-martin'
 
+    size_sweep = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768]
+    # size_sweep = [2048, 4090, 4093, 4096, 4099, 4102]
+    # size_sweep = [16, 32, 64, 128, 256, 512, 1024, 2048, 8192, 16384, 32768]
 
-    #
-    # Sequence of pings over range of packet sizes.
-    #
-    stats_sweep = ping_sweep(host_name, count_send=25, time_pause=5.)
-
-    pretty_results(stats_sweep)
+    count_send = 25
+    
+    # Do it: sequence of pings over range of packet sizes.
+    stats_sweep = ping_sweep(host_name, size_sweep=size_sweep, count_send=count_send, verbosity=True)
