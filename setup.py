a06f1382d54ae4ac40db786d957eaf40fb862e3e
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2013-08-11 13:02:56 -0700

    Update setup.py Remove __init__.py from reference_ideas folder.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup, find_packages
 entry_points = {'console_scripts': ['ping_sweep = ping_sweep:main']}
 
 # Other stuff.
-version = '2013.08.10'
+version = '2013.08.11'
 description = 'Ping return time analysis tool'
 
 # Do it.
 setup(name='ping_sweep',
       packages=find_packages(),
-      
+
       entry_points=entry_points,
 
       # Metadata
